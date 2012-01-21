6042df791639b91e37d4e63a9d01e28ec86ff4dd
Neetu Mishra <neetumis@thoughtworks.com>
2012-01-21 09:38:14 +0530

    first commit

---
+++ src/code/StartApp.java
@@ -0,0 +1,16 @@
+package code;
+
+import java.io.IOException;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: neetu
+ * Date: 21/1/12
+ * Time: 9:17 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class StartApp {
+    public static void main(String[] args) throws IOException{
+
+    }
+}
